43434c50d5e9eb8f484dc8b50846430c10a25084
RainBWP <114127654+RainBWP@users.noreply.github.com>
2025-05-10 03:37:05 -0600

    Unused components

--- reconstruccion/src/App.tsx
+++ reconstruccion/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import AlgoritmosGeneticos from './components/AlgoritmosGeneticos';
 import FeedbackNN from './components/FeedbackNN';
 import Main from './components/Main';
